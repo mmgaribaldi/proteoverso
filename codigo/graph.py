0f4d8e113057dde1622d21e65fa007069360a288
Martin Garibaldi <martin.garibaldi@gmail.com>
2018-10-17 08:11:31 -0300

    Agregados 0.65

--- codigo/graph.py
+++ codigo/graph.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.interpolate import BSpline
 
 # Leo los resultados a graficar
 procesados = open("../resultados/agraficar.txt", "r")
@@ -13,16 +13,24 @@ for j, elem in enumerate(flias):
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
         i = 0
-        x = np.zeros(11)
-        y = np.zeros(11)
+        x = np.zeros(12)
+        y = np.zeros(12)
         for row in spamreader:
             x[i] = row[1]
             y[i] = row[4]
             i = i+1
+        x[11] = 1
+        y[11] = 1
 
         plt.title('Familia: ' + str(elem[8:-10]))
-        plt.plot([x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10]], [y[0],y[1],y[2],y[3],y[4],y[5],y[6],y[7],y[8],y[9],y[10]])
+
+        plt.plot([x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11]], [y[0],y[1],y[2],y[3],y[4],y[5],y[6],y[7],y[8],y[9],y[10],y[11]])
         plt.xlabel('Valores de corte')
         plt.ylabel('# clusters normalizados')
-        plt.savefig(str(elem) + ".png")
-        plt.close()
+
+        if j == 100:
+            plt.savefig(str(elem) + ".png")
+            plt.close()
+
+plt.savefig(str(elem) + ".png")
+plt.close()
