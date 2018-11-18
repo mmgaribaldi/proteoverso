9cfda651f65f132384d8cffeb76ac1978b765bec
Martin Garibaldi <martin.garibaldi@gmail.com>
2018-11-18 12:22:53 -0300

    Add cd-hit vs heni

--- codigo/cdhitVsHenikoff.py
+++ codigo/cdhitVsHenikoff.py
@@ -4,9 +4,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Leo los resultados a graficar
-procesados = open("../resultados/agraficar.txt", "r")
-flias = procesados.readlines()
+# Leo los cdhit a graficar
+procesadosCdhit = open("../resultados/agraficar-cdhit.txt", "r")
+flias = procesadosCdhit.readlines()
 
 ejeX = np.zeros(len(flias))
 
@@ -18,6 +18,32 @@ for j, elem in enumerate(flias):
         for row in spamreader:
             if row[1] == '0.95':
                 introw=math.log(float(row[3]),10)
+                #introw = float(row[3])
                 ejeX[j] = introw
 
 print(ejeX)
+
+# Leo los resultados a graficar
+procesadosHeni = open("../resultados/agraficar-heni.txt", "r")
+flias = procesadosHeni.readlines()
+
+ejeY = np.zeros(len(flias))
+
+for j, elem in enumerate(flias):
+
+    with open('../resultados/' + str(elem).rstrip(), newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter='|', quotechar='"')
+
+        for row in spamreader:
+            introw=math.log(float(row[4]),10)
+            #introw = float(row[4])
+            ejeY[j] = introw
+
+print(ejeY)
+
+plt.title('Cd-hit vs Henikoff: ')
+plt.scatter(ejeX,ejeY)
+
+plt.xlabel('Log(cd-hit con clusters al 95%)')
+plt.ylabel('Log(Secuencias efectivas Henikoff')
+plt.show()
